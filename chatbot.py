3913c3563e206ef829a37930a274e80d19a887ea
Saksham Mehrotra <142745420+sakshamm0507@users.noreply.github.com>
2024-12-04 01:03:08 +0530

    Create chatbot.py
    It is a basic chatbot using gemini api and python

---
+++ chatbot.py
@@ -0,0 +1,23 @@
+import os
+import google.generativeai as genai
+
+Api_key="AIzaSyB6GiOxhoNT-j0FeG7UrJELciZ0kEooMmE"
+genai.configure(api_key=Api_key)
+
+
+for m in genai.list_models():
+  if 'generateContent' in m.supported_generation_methods:
+    print(m.name)
+
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+
+while True:
+    prompt = input("Ask me anything: ")
+    if (prompt == "exit"):
+       
+        break
+    response = chat.send_message(prompt, stream=True)
+    for chunk in response:
+        if chunk.text:
+          print(chunk.text)
